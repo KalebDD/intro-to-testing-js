3b255dca3d5547b3dfc142edc5541007e41ec9e3
Kaleb Dworsky <dworsky.kaleb@gmail.com>
2020-02-11 11:16:19 -0600

    finished exercise 9

--- tests.js
+++ tests.js
@@ -13,28 +13,35 @@ describe('helloWorld', function() {
       expect(helloWorld()).not.toBe(undefined);
     });
 });
-
+// Unit tests for sayHello function
 describe('sayHello', function() {
     it('should be a defined function', function() {
         expect(typeof sayHello).toBe('function');
-    })
+    });
     it('should return a string when called', function() {
         expect(typeof sayHello(undefined)).toBe('string');
-    })
+    });
     it('an argument of "Jane" should return  Hello, Jane!', function() {
         expect(sayHello('Jane')).toBe("Hello, Jane!");
-    })
+    });
     it('an argument of "Alex" should return Hello, Alex!', function() {
         expect(sayHello('Alex')).toBe("Hello, Alex!");
-    })
+    });
     it('an argument of "Pat" should return Hello, Pat!', function() {
         expect(sayHello('Pat')).toBe("Hello, Pat!");
-    })
+    });
     it('return Hello, World if no argument is provided', function() {
         expect(sayHello()).toBe('Hello, World!');
-    })
+    });
     it('a boolean argument should return Hello, World!', function() {
         expect(sayHello(Boolean)).toBe("Hello, World!");
-    })
-})
+    });
+});
+
+// Unit tests for isFive function
+describe('isFive', function() {
+    it('should be a defined function', function() {
+        expect(typeof isFive).toBe('function');
+    });
+});
 
